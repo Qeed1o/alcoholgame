6e54174bf28e48b7b38e9eda5e6f87d25bf858d1
Qeed1o <sergeykavai@gmail.com>
2020-07-04 17:58:08 +0200

    Govno

--- main.js
+++ main.js
@@ -3,17 +3,30 @@ window.onload = () => { }
 let namesArray = [];
 let alcoArray = [];
 
+let nameIndex = 0;
+let alcoIndex = 0;
+
 const addName = () => {
     const input = document.getElementById('name');
+    const select = document.getElementById('names');
     console.log(`AddName: ${input.value}`);
     namesArray.push(input.value);
+    const optionToAdd = document.createElement('option');
+    optionToAdd.value = input.value;
+    optionToAdd.text = input.value;
+    select.appendChild(optionToAdd);
     input.value = "";
 }
 
 const addAlco = () => {
     const input = document.getElementById('alco');
+    const select = document.getElementById('alcos');
     console.log(`AddAlco: ${input.value}`);
     alcoArray.push(input.value);
+    const optionToAdd = document.createElement('option');
+    optionToAdd.value = input.value;
+    optionToAdd.text = input.value;
+    select.appendChild(optionToAdd);
     input.value = "";
 }
 
@@ -27,28 +40,36 @@ const animation = (el, callback) => {
 }
 
 const select = (el) => {
-   const isRocket = el.classList[0] === "fa";
+   const isRocket = el.id === "dirik";
+//    alcoIndex = ((alcoIndex + 1) <= (alcoArray.length - 1)) ? alcoIndex + 1 : 0;
 
    if(isRocket){
        animation(el, function(){
             document.getElementById('winner').style.color = "black"; 
-
-            const rAlco = alcoArray[Math.floor(Math.random() * alcoArray.length)]
-            const rName = namesArray[Math.floor(Math.random() * namesArray.length)]
+            alcoIndex = Math.floor(alcoArray.length - 1);
+            const rAlco = alcoArray[alcoIndex]
+            const rName = namesArray[nameIndex]
+            console.log(nameIndex, namesArray[nameIndex])
             document.getElementById('winner').innerHTML = `${rAlco} + ${rName} = <i class="fa fa-heart" aria-hidden="true"></i>`;
 
             document.getElementById('winner').style.display = "";
-            document.getElementById('rocket').style.display = "none";
+            document.getElementById('dirik').style.display = "none";
+            nameIndex = nameIndex + 1;
+            nameIndex >= namesArray.length ? nameIndex = 0 : {};
        });
    }else{
         animation(el, function(){
             document.getElementById('winner').style.display = "none";
-            document.getElementById('rocket').style.display = "";
+            document.getElementById('dirik').style.display = "";
         });
    }
+
+   
 }
 
 const reset = () => {
     namesArray = [];
     alcoArray = [];
+    document.getElementById('names').innerHTML = ""
+    document.getElementById('alcos').innerHTML = ""
 }
